bbd0d86c0899a98725cb33ecb26a3d9ca15d2deb
ceemion <fiyinsimeon@gmail.com>
2019-11-24 22:26:35 +0100

    Initial Commit

---
+++ hoplon/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  hoplon
+//
+//  Created by Fiyinfoluwa Adebayo on 24/11/2019.
+//  Copyright © 2019 Kompilab Limited. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
